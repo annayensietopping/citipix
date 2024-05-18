ad1b282385a8e161f17aad006da41a79703cd7c8
Anna Yensie Topping <47766564+annayensietopping@users.noreply.github.com>
2024-05-18 15:55:01 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,23 @@
-$(function () {
+$(document).ready(function() {
+    $('#submit-btn').click(function() {
+        // Get the user's input from the text input field
+        var city = $('#city-type').val();
+
+        if (city === "New York" || city === "New York City" || city === "NYC") {
+            // Change the background image based on the city name entered by the user
+          //  ../images/citipix_skyline.jpg
+            $('body').css('background-image', 'url(../images/nyc.jpg)');
+        } else if (city === "San Francisco" || city === "SF" || city === "Bay Area") {
+            $('body').css('background-image', 'url(../images/sf.jpg)');
+        } else if (city === "Los Angeles" || city === "LA" || city === "LAX") {
+            $('body').css('background-image', 'url(../images/la.jpg)');
+        } else if (city === "Austin" || city === "ATX") {
+            $('body').css('background-image', 'url(../images/austin.jpg)');
+        } else if (city === "Sydney" || city === "SYD") {
+            $('body').css('background-image', 'url(../images/sydney.jpg)');
+        } else {
+            // If the city is not recognized, set a default background or do nothing
+            $('body').css('background-image', 'url(../images/citipix_skyline.jpg)');
+        }
+    });
 })
